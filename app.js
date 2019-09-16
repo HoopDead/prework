2f4f63c36f0444b4414ef0cbf5e3fb3b84776543
hoopdead <polec0@wp.pl>
2019-09-17 00:53:56 +0200

    Second attempt
    Second attempt in code - added function to apiRefresh every 1000 ms.

--- app.js
+++ app.js
@@ -1,33 +1,46 @@
-let currencies = ["LML-BTC", "LTC-USD", "BSV-EUR"];
-
-let requests = currencies.map(request =>
-  fetch(`https://api.bitbay.net/rest/trading/ticker/${request}`)
-);
-
-Promise.all(requests)
-  .then(responses => {
-    return responses;
-  })
-  .then(responses => Promise.all(responses.map(callback => callback.json())))
-  .then(currencies =>
-    currencies.forEach(request =>
-      createCard(
-        "Kod marketu: " +
-          request.ticker.market.code +
-          "<br>(" +
-          request.ticker.market.first.currency +
-          ") Min Offer: " +
-          request.ticker.market.first.minOffer +
-          "<br>( " +
-          request.ticker.market.second.currency +
-          ") Min Offer: " +
-          request.ticker.market.second.minOffer +
-          "<br>Highest bid: " +
-          request.ticker.highestBid
-      )
-    )
+const apiCall = () => {
+  const request = new Request(
+    "https://api.bitbay.net/rest/trading/ticker/ALG-BTC",
+    {
+      method: "GET",
+      headers: { "content-type": "application/json" }
+    }
   );
 
-const createCard = value => {
-  $(`<p>${value}</p>`).appendTo(document.body);
+  fetch(request)
+    .then(response => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error("Something went wrong on api server!");
+      }
+    })
+    .then(response => {
+      console.log(response);
+      createElement(
+        "Kod marketu: " +
+          response.ticker.market.code +
+          "<br>" +
+          "Highest bid: " +
+          response.ticker.highestBid +
+          "<br>Lowest ask: " +
+          response.ticker.lowestAsk
+      );
+    })
+    .catch(error => {
+      console.error(error);
+    });
 };
+
+const createElement = value => {
+  $("#values").html(`${value}`);
+};
+
+const apiRefresh = () => {
+  setInterval(function() {
+    apiCall();
+  }, 1000);
+};
+
+apiCall();
+apiRefresh();
